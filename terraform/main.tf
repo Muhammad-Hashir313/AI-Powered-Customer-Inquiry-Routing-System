64876cd9dfdef0631d48ca809a45d761f0c1f8bd
Muhammad Hashir <mehashir313@gmail.com>
2026-04-16 20:24:22 +0500

    Added s3 and cloudfront setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,3 @@
-resource "aws_key_pair" "tf-key" {
-  key_name   = "tf-key"
-  public_key = file(var.aws_key_pair_destination)
-}
-
 # VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block       = "10.0.0.0/16"
@@ -64,4 +59,79 @@ resource "aws_route_table_association" "public-rt-association" {
 resource "aws_route_table_association" "private-rt-association" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private-rt.id
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "ai-powered-customer-inquiry-system-bucket"
+}
+
+# OAC for CloudFront
+resource "aws_cloudfront_origin_access_control" "my-oac" {
+  name                              = "s3-oac"
+  description                       = "OAC for S3 CloudFront distribution"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = aws_s3_bucket.my_bucket.bucket_regional_domain_name
+    origin_id                = "s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.my-oac.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_cloudfront" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.my_bucket.arn}/*"
+
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
+          }
+        }
+      }
+    ]
+  })
 }
